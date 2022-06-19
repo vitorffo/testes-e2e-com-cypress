5e3b5fa9d73aa413dc3c5166b771ea91e3e5b970
Vitor <fernandofernandesvitor@gmail.com>
2022-06-19 01:02:29 -0400

    docs added

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -1,4 +1,4 @@
-// cypress/integration/signup.spec.js
+/// <reference path="../support/commands.d.ts" />
 
 it('successfully signs up using confirmation code sent via email', () => {
   const faker = require('faker')
